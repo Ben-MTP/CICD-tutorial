c16b688a6df234da28c1e91229cf950fccc782dd
Ben-MTP <minhmanh.office.uet@gmail.com>
2022-11-21 16:08:35 +0700

    Init project cicd in github

---
+++ src/main/java/com/msoft/core/CicdTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.msoft.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTutorialApplication.class, args);
+	}
+
+}
